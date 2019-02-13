2ecd1571418ee220b20990e9f9bf69401137e28c
MolbOrg <MolbOrg@gmail.com>
2019-02-13 03:40:04 +0300

    performance monitor looks for active camera

--- scripts/Performance.gd
+++ scripts/Performance.gd
@@ -2,6 +2,8 @@ extends Control
 
 export(NodePath) var source_xyz = "Camera"
 
+func findACamera():
+	pass
 
 func _process(delta):
 	$Panel/FPS.text = str(Performance.get_monitor(Performance.TIME_FPS))
@@ -9,10 +11,22 @@ func _process(delta):
 	$Panel/Vertices.text = str(Performance.get_monitor(Performance.RENDER_VERTICES_IN_FRAME))
 	$Panel/VRAM.text = str(Performance.get_monitor(Performance.RENDER_VIDEO_MEM_USED))
 	
-	var xyz = self.get_node(source_xyz)
-	
-	if xyz :
-		var ORIGIN = xyz.to_global(xyz.transform.origin) #Don't change this line or the coordinates won't work if camera is child of moving object (player)
-		$Panel/x.text = str(ORIGIN.x)
-		$Panel/y.text = str(ORIGIN.y)
-		$Panel/z.text = str(ORIGIN.z)
+	if self.has_node(source_xyz):
+		var xyz = self.get_node(source_xyz)
+		
+		if xyz :
+			var ORIGIN = xyz.to_global(xyz.transform.origin) #Don't change this line or the coordinates won't work if camera is child of moving object (player)
+			$Panel/x.text = str(ORIGIN.x)
+			$Panel/y.text = str(ORIGIN.y)
+			$Panel/z.text = str(ORIGIN.z)
+	else:
+		var root = get_tree().current_scene
+		var cameras = utils.get_nodes_type(root, "Camera", true)
+		print("**********", cameras)
+		for p in cameras:
+			var cam = root.get_node(p)
+			print(p, " ", cam.current)
+			if cam.current:
+				source_xyz = self.get_path_to(cam)
+				break
+
