2b182e5a6ee31d814c0ea9b106ca4cb8e06f2dce
MolbOrg <MolbOrg@gmail.com>
2019-02-18 05:28:31 +0300

    hide objects respecting group protecion

--- debug.gd
+++ debug.gd
@@ -52,10 +52,26 @@ func e_collision_shapes(enable=true):
 		var obj = root.get_node(p)
 		obj.disabled = !enable
 
+func hide_obj_check(root, path):
+	var obj = root.get_node(path)
+	var hide = true
+	if utils.obj_has_groups(obj, utils.cs_options.hide_protect):
+		hide = false
+	if hide and obj.get_child_count() > 0:
+		var nodes = utils.get_nodes_type(obj, "MeshInstance", true)
+		for p in nodes:
+			if obj.get_node(p).visible:
+				hide = false
+				break
+	return hide
+
+
 #Hide MeshInstance nodes with a chance defined by probability
 var hidden_nodes = []
-func hide_nodes_random(probability):
+func hide_nodes_random(probability=null):
 	var root = get_tree().current_scene
+	if probability == null:
+		probability = options.get("decimate", "probability", 80)
 	if probability == 0:
 		#unhide all nodes
 		print("unhide nodes (%s)" % hidden_nodes.size())
@@ -63,12 +79,15 @@ func hide_nodes_random(probability):
 			root.get_node(p).visible = true
 		hidden_nodes = []
 		return
+	
 	var nodes = utils.get_nodes_type(root, "MeshInstance", true)
 	print("hide nodes, total(%s) already hidden(%s) probability(%s)" % [nodes.size(), hidden_nodes.size(), probability])
-	
+	if nodes.size() < 1 :
+		return
 	for p in nodes:
 		if not hidden_nodes.has(p):
 			var hide = (randi() % 100 <= probability)
-			if hide:
+			if hide and hide_obj_check(root, p):
 				root.get_node(p).visible = false
 				hidden_nodes.append(p)
+	print("hide nodes, total(%s) already hidden(%s) probability(%s)" % [nodes.size(), hidden_nodes.size(), probability])
