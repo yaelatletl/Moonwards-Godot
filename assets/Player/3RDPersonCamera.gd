b24cfa8bd34003f179b7289cb8ae5aea0dfda5f3
yaelatletl <yael.atletl@gmail.com>
2019-03-07 21:59:38 -0600

    3rd person camera added

---
+++ assets/Player/3RDPersonCamera.gd
@@ -0,0 +1,43 @@
+extends KinematicBody
+
+# class member variables go here, for example:
+var linear_velocity = Vector3()
+var gravity = Vector3(0,1,0)
+export(NodePath) var Target 
+#var originalpos = Vector3()
+export(NodePath) var origin 
+#var cameramovement
+func _enter_tree():
+	#cameramovement = get_node("../..").fixpos
+	origin = get_node(origin).translation
+	Target = get_node(Target).translation
+func _process(delta):
+	linear_velocity = origin - translation
+	#move_and_slide(linear_velocity,-gravity.normalized())
+	
+	
+	translation.x = clamp(translation.x, Target.x-0.5, Target.x+0.5)
+	translation.y = clamp(translation.y, Target.y-0.5, Target.y+0.5)
+	translation.z = clamp(translation.z, Target.z-0.5, Target.z+0.5)
+	
+	
+	
+	if translation != origin: #and not (is_on_floor() or is_on_ceiling() or is_on_wall()):
+		
+		move_and_slide(linear_velocity,-gravity.normalized())
+		#if translation.x <= origin.x-0.2:
+	#		translation.x = translation.x + delta
+	#	if translation.x >= -origin.x+0.2:
+	#		translation.x = translation.x - delta
+	#		
+	#	if translation.y <= origin.y-0.2:
+	#		translation.y = translation.y + delta
+	#	if translation.y >= origin.y+0.2:
+	#		translation.y = translation.y - delta
+	#		
+	#	if translation.z <= origin.y-0.2:
+	#		translation.z = translation.z + delta
+	#	if translation.z >= origin.y+0.2:
+	#		translation.z = translation.z - delta	
+			
+	
