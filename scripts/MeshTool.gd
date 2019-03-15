a5bef4ee21853171af4a46908658a9b12d152ded
MolbOrg <MolbOrg@gmail.com>
2019-03-15 21:18:31 +0300

    fix scripts/MeshTool.gd

--- scripts/MeshTool.gd
+++ scripts/MeshTool.gd
@@ -1,14 +1,20 @@
 extends Node
 
+export(String) var id = "test"
 var mesh setget set_mesh, get_mesh
 var mesh_info = {}
 var mesh_cache = {}
 var root
+
 func cache_vars(id=null):
-	if mesh:
-		mesh_cache[id_mesh(mesh)] = mesh_info
+	if id == null and mesh != null:
+		id = id_mesh(mesh)
+		mesh_cache[id] = mesh_info
+		return
+	
 	if id and mesh_cache.has(id):
 		mesh_info = mesh_cache[id]
+		#print("cache_vars hit %s" % id)
 	else:
 		mesh_info = {}
 
@@ -16,8 +22,8 @@ func reset_vars():
 	mesh_info = {}
 
 func set_mesh(obj):
-	if ! obj:
-		print("set_mesh - obj == null")
+	if obj == null:
+		print("set_mesh, obj == null")
 		return
 	if root == null:
 		print("scene tree is not defined")
@@ -33,12 +39,16 @@ func set_mesh(obj):
 	if obj is Mesh:
 		mesh = obj
 	cache_vars(id_mesh(mesh))
-	print("MeshTool(%s)" % mesh)
+	#print("MeshTool(%s)" % mesh)
 
 func get_mesh():
 	return mesh
 
-func _init(tree, obj=null):
+func _init(tree=null, obj=null):
+	print("MeshTool::init(%s, %s)" % [tree, obj])
+	if tree == null:
+		if get_tree():
+			tree = get_tree().current_scene
 	root = tree
 	set_mesh(obj)
 
@@ -59,9 +69,10 @@ func get_median():
 	
 func get_hitbox():
 	if mesh == null:
-		return null
+		print("get_hitbox mesh is null")
+		return
 	if mesh_info.has("hitbox"):
-		return mesh.hitbox
+		return mesh_info.hitbox
 	
 	var box  #x, -x y, -y, z, -z
 	for v in mesh.get_faces():
@@ -85,10 +96,14 @@ func get_hitbox():
 
 func hbox_volume():
 	var bb = get_hitbox()
+	if bb == null:
+		return 0
 	return bb[1].x*bb[1].y*bb[1].z
 
 func hbox_surface():
 	var bb = get_hitbox()
+	if bb == null:
+		return 0
 	var hs = bb[1]
 	return 2*hs.x*hs.y+2*hs.x*hs.z + 2*hs.y*hs.z
 
@@ -106,8 +121,10 @@ func id_mesh(mesh_obj=null):
 	if mesh_obj != null:
 		obj = mesh_obj
 	if obj == null:
+		print("id_mesh, object is null")
 		return null
-	if not obj.is_class("MeshInstance"):
+	if not obj.is_class("Resource"):
+		print("id_mesh obj(%s) not a Resource type" % obj)
 		return null
-	var path = obj.resource_path()
+	var path = obj.resource_path
 	return path.md5_text()
