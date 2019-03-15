0ef3cd36ce4eae5ee82e826d0c4b2e13df336a23
MolbOrg <MolbOrg@gmail.com>
2019-03-15 21:19:34 +0300

    disable InputMapping messing

--- assets/UI/Menu/Controls_settings.gd
+++ assets/UI/Menu/Controls_settings.gd
@@ -3,6 +3,7 @@ extends Tabs
 
 
 func _on_Button_pressed():
-	for action in InputMap.get_actions():
-		if InputMap.get_action_list(action)[0] is InputEventKey:
-				InputMapping.save_to_config("input", action, InputMap.get_action_list(action)[0].scancode)
+#	for action in InputMap.get_actions():
+#		if InputMap.get_action_list(action)[0] is InputEventKey:
+#				InputMapping.save_to_config("input", action, InputMap.get_action_list(action)[0].scancode)
+	pass
