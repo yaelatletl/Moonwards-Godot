fccd33f3c1553590bd430aef9c1e5066c4edf383
MolbOrg <MolbOrg@gmail.com>
2019-03-18 20:57:04 +0300

    fix mouse apperance when menu opened

--- UserInterface.gd
+++ UserInterface.gd
@@ -14,10 +14,12 @@ func OptionsPanel():
 			Options.queue_free()
 			Options = null
 			options.save()
+			Input.set_mouse_mode(options.get("_state_", "menu_options_mm", Input.MOUSE_MODE_VISIBLE))
 		else:
 			Options = ResourceLoader.load(SceneOptions).instance()
 			Options.signal_close = true
 			Options.connect("close", self, "OptionsPanel")
 			Options.set_tab_index(options.get("_state_", "menu_options_tab", 1))
 			get_tree().get_root().add_child(Options)
-			
+			options.set("_state_", Input.get_mouse_mode(), "menu_options_mm")
+			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
