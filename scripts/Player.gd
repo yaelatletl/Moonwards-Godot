874a64e5f4266313df42ad06d0e0578fff508eeb
Gyrth <gyrthmcmulin@gmail.com>
2019-04-06 21:42:04 +0200

    Added networking functionality to the new character script. Added nametags to players.

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -1,7 +1,8 @@
 extends Spatial
 
+export(bool) var remote_player = false
 export(NodePath) var camera_control_path
-onready var camera_control = get_node(camera_control_path)
+onready var camera_control = null
 
 const MOTION_INTERPOLATE_SPEED = 10
 const ROTATION_INTERPOLATE_SPEED = 10
@@ -14,21 +15,39 @@ var max_down_aim_angle = 55.0
 var root_motion = Transform()
 var orientation = Transform()
 var velocity = Vector3()
-var run = false
+var running = false
 var in_air = false
 var land = false
 var jumping = false
+var network = false
+var flies = false
 var movementstate = walk
+var username = "username"
+var id = ""
 
 const GRAVITY = Vector3(0,-1.62, 0)
 const JUMP_SPEED = 0.75
 const walk = 0
 const flail = 1
 
+##Networking
+var puppet = false
+puppet var puppet_translation
+puppet var puppet_rotation
+puppet var puppet_jump
+puppet var puppet_run
+puppet var puppet_motion
+
+var nonetwork = false
+
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	orientation = $KinematicBody/Model.global_transform
 	orientation.origin = Vector3()
+	if not remote_player:
+		camera_control = get_node(camera_control_path)
+	else:
+		set_process_input(false)
 
 func _input(event):
 	if (event is InputEventMouseMotion):
@@ -51,19 +70,33 @@ func Jump():
 	velocity.y += JUMP_SPEED
 
 func _physics_process(delta):
-	
 	if $KinematicBody/OnGround.is_colliding() and in_air:
 		in_air = false
 		land = true
 	elif not $KinematicBody/OnGround.is_colliding() and not in_air:
 		in_air = true
 	
-	HandleMovement(delta)
-	HandleJump()
+	HandleControls(delta)
+	UpdateNetworking()
+	HandleMovement()
 
-func HandleMovement(var delta):
+func HandleMovement():
+	$KinematicBody/AnimationTree["parameters/Walk/blend_position"] = motion
+	if jumping:
+		$KinematicBody/AnimationTree["parameters/Jump/active"] = true
+	if running:
+		$KinematicBody/AnimationTree["parameters/MovementSpeed/scale"] = 3.0
+	else:
+		$KinematicBody/AnimationTree["parameters/MovementSpeed/scale"] = 1.0
+
+func HandleControls(var delta):
+	if remote_player:
+		return
 	var motion_target = Vector2( 	Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
 									Input.get_action_strength("move_forwards") - Input.get_action_strength("move_backwards"))
+	
+	if Input.is_action_just_pressed("jump") and not in_air and not jumping:
+		jumping = true
 	
 	if in_air and movementstate == walk:
 		$KinematicBody/AnimationTree["parameters/MovementState/current"] = flail
@@ -81,16 +114,12 @@ func HandleMovement(var delta):
 		motion = motion.linear_interpolate(motion_target, MOTION_INTERPOLATE_SPEED * delta)
 	else:
 		pass
-	$KinematicBody/AnimationTree["parameters/Walk/blend_position"] = motion
 	
 	#Multiply the movement animation when running. The animation takes care of the actual movement speed.
 	if Input.is_action_pressed("move_run"):
-		if not run:
-			$KinematicBody/AnimationTree["parameters/MovementSpeed/scale"] = 3.0
-			run = true
-	elif run:
-		$KinematicBody/AnimationTree["parameters/MovementSpeed/scale"] = 1.0
-		run = false
+		running = true
+	else:
+		running = false
 	
 	#Update the model rotation based on the camera look direction.
 	var target_direction = -camera_control.camera.global_transform.basis.z
@@ -120,7 +149,42 @@ func HandleMovement(var delta):
 	if motion_target != Vector2():
 		$KinematicBody/Model.global_transform.basis = orientation.basis
 
-func HandleJump():
-	if Input.is_action_just_pressed("jump") and not in_air and not jumping:
-		jumping = true
-		$KinematicBody/AnimationTree["parameters/Jump/active"] = true
+func UpdateNetworking():
+	if not network:
+		return
+	if remote_player:
+		if puppet_translation != null:
+			$KinematicBody.global_transform.origin = puppet_translation
+		if puppet_rotation != null:
+			$KinematicBody/Model.global_transform.basis = puppet_rotation
+		if puppet_jump != null:
+			jumping = puppet_jump
+		if puppet_motion != null:
+			motion = puppet_motion
+		if puppet_run != null:
+			running = puppet_run
+	else:
+		rset_unreliable("puppet_translation", $KinematicBody.global_transform.origin)
+		rset_unreliable("puppet_rotation", $KinematicBody/Model.global_transform.basis)
+		rset_unreliable("puppet_motion", motion)
+		rset_unreliable("puppet_jump", jumping)
+		rset_unreliable("puppet_run", running)
+
+func SetID(var _id):
+	id = _id
+
+func SetUsername(var _username):
+	username = _username
+	$KinematicBody/Nametag/Viewport/Username.text = username
+
+func SetNetwork(var enabled):
+	network = enabled
+
+	if network:
+		rset_config("puppet_translation", MultiplayerAPI.RPC_MODE_PUPPET)
+		rset_config("puppet_rotation",  MultiplayerAPI.RPC_MODE_PUPPET)
+		rset_config("puppet_jump",  MultiplayerAPI.RPC_MODE_PUPPET)
+	else:
+		rset_config("puppet_translation", MultiplayerAPI.RPC_MODE_DISABLED)
+		rset_config("puppet_rotation",  MultiplayerAPI.RPC_MODE_DISABLED)
+		rset_config("puppet_jump", MultiplayerAPI.RPC_MODE_DISABLED)
