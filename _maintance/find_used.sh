65ab553775e5271e485ea0a7c2b3a6a42ac5c367
MolbOrg <MolbOrg@gmail.com>
2019-04-08 20:42:24 +0300

    maintanance script

---
+++ _maintance/find_used.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd `
+DIR=`dirname $DIR`
+
+echo "search in" $DIR
+
+#find $DIR -iname '*.tscn' -iname '*.gd' -type f -print0 | xargs -0 echo grep $1
+find $DIR \( -iname '*.tscn' -or -iname '*.gd' \) -type f -print0 | xargs -0 grep $1
