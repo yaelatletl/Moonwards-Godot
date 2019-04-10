7758c26d2cd282f19e26b885e428bec45b342edb
MolbOrg <MolbOrg@gmail.com>
2019-04-10 19:18:22 +0300

    fix script

--- _maintance/find_used.sh
+++ _maintance/find_used.sh
@@ -3,6 +3,11 @@
 DIR=`cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd `
 DIR=`dirname $DIR`
 
+if [ -z "$1" ] ; then
+    echo "search pattern needed"
+    exit 0
+fi
+
 echo "search in" $DIR
 
 #find $DIR -iname '*.tscn' -iname '*.gd' -type f -print0 | xargs -0 echo grep $1
