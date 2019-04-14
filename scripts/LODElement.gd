00fb1fde87492915b9866138695b6dea43742d5f
MolbOrg <MolbOrg@gmail.com>
2019-04-14 18:13:01 +0300

    make lod element a bit smarter

--- scripts/LODElement.gd
+++ scripts/LODElement.gd
@@ -1,7 +1,44 @@
 extends Spatial
 
 export (NodePath) var substitute
+const id = "LODElement"
+func printd(s):
+	utils.printdd(id, s)
+
+var state
 
 func _ready():
 	print("LOD substitute %s of %s" % [get_path(), substitute] )
 	add_to_group("LODElement", true)
+	set_state()
+
+func set_state():
+	if state == null:
+		save_state()
+	if state == null:
+		printd("fail to substitute %s %s %s" % [utils.get_node_file(self), get_path(), substitute])
+	var node = get_node(substitute)
+	if node.visible:
+		node.visible = false
+		printd("hide %s" % substitute)
+	if not visible:
+		visible = true
+		printd("show %s" % name)
+	
+
+func save_state():
+	var node = get_node(substitute)
+	if node != null:
+		state = {
+			svisible = node.visible,
+			visible = visible
+		}
+
+func restore():
+	if state:
+		var node = get_node(substitute)
+		if node:
+			if node.visible != state.svisible:
+				node.visible = state.svisible
+			if visible != state.visible:
+				visible = state.visible
