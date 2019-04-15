f47551c99a07b0c6688f7d1536f6efb8ec7ccf7c
MolbOrg <MolbOrg@gmail.com>
2019-04-16 00:30:37 +0300

    disable AreaLod for time being

--- scripts/AreaLod.gd
+++ scripts/AreaLod.gd
@@ -1,6 +1,7 @@
 extends Area
 
 export(NodePath) var RootNode
+export(bool) var enabled = false
 export(String) var showhide = "interior"
 export(String) var hideshow = "exterior"
 export(String) var bygroup = "player"
@@ -9,6 +10,9 @@ var root
 var nodes_sh
 var nodes_hs
 var id_path
+
+func printd(s):
+	utils.printdd("AreaLod", s)
 
 func array_add(a, b):
 	for i in b:
@@ -39,23 +43,35 @@ func get_node_list(root, groups):
 	return match_obj
 
 func _ready():
+	if not enabled:
+		printd("AreaLod disabled at %s %s" % [get_path(), utils.get_node_root(self)])
+	init_all()
+
+func init_all():
 	if not RootNode :
 		root = self.get_parent()
 	else:
 		root = self.get_node(RootNode)
 	nodes_sh = get_node_list(root, [showhide])
 	nodes_hs = get_node_list(root, [hideshow])
-	connect("area_entered", self, "_on_Area_area_entered")
-	connect("area_exited", self, "_on_Area_area_exited")
+# 	connect("area_entered", self, "_on_Area_area_entered")
+# 	connect("area_exited", self, "_on_Area_area_exited")
+	connect("body_exited", self, "_on_body_exit")
+	connect("body_entered", self, "_on_body_enter")
 	id_path = get_path()
-	print("Area ready: %s" % id_path)
+	printd("Area ready: %s" % id_path)
 	area_exit("on ready")
 	
+func _on_body_enter(body):
+	printd("body_enter %s" % body)
+
+func _on_body_exit(body):
+	printd("body_exit %s" % body)
 
 func _on_Area_area_entered(area):
 	if not bygroup in area.get_groups():
 		return
-	print("Area enter: %s" % id_path)
+	printd("Area enter: %s" % id_path)
 	for obj in nodes_sh:
 		obj.visible = true
 	for obj in nodes_hs:
@@ -63,7 +79,7 @@ func _on_Area_area_entered(area):
 
 func area_exit(s=""):
 	if s != "on ready":
-		print("Area exit: %s" % id_path)
+		printd("Area exit: %s" % id_path)
 	for obj in nodes_sh:
 		obj.visible = false
 	for obj in nodes_hs:
