84ee5c03d4644c3cbd5aaa20dc58fa04dfddcac6
Gyrth <gyrthmcmulin@gmail.com>
2019-05-27 18:33:18 +0200

    Added scroll to media mode camera to control movement speed. And mouse doesn't have to be held anymore.

--- scripts/media_mode_camera.gd
+++ scripts/media_mode_camera.gd
@@ -9,10 +9,10 @@ var max_down_aim_angle = 80.0
 var mouse_sensitivity = 0.10
 var velocity = Vector3()
 var look_velocity = Vector3()
-var mouse_down = false
 
 var velocity_damp = 0.95
-var acceleration = 0.2
+var look_acceleration = 0.2
+var movement_acceleration = 0.2
 
 func _process(delta):
 	var forward_direction = global_transform.basis.z
@@ -29,35 +29,31 @@ func _process(delta):
 	elif rotation_degrees.x < -max_down_aim_angle:
 		rotation_degrees.x = -max_down_aim_angle
 	
-	if Input.is_action_pressed("left_click"):
-		mouse_down = true
-	else:
-		mouse_down = false
-	
 	if Input.is_action_pressed("move_forwards"):
 		if Input.is_action_pressed("shift"):
-			velocity -= Vector3(0, -1, 0) * delta * acceleration
+			velocity -= Vector3(0, -1, 0) * delta * movement_acceleration
 		else:
-			velocity -= forward_direction * delta * acceleration
+			velocity -= forward_direction * delta * movement_acceleration
 	
 	if Input.is_action_pressed("move_backwards"):
 		if Input.is_action_pressed("shift"):
-			velocity += Vector3(0, -1, 0) * delta * acceleration
+			velocity += Vector3(0, -1, 0) * delta * movement_acceleration
 		else:
-			velocity += forward_direction * delta * acceleration
+			velocity += forward_direction * delta * movement_acceleration
 	
 	if Input.is_action_pressed("move_left"):
-		velocity -= right_direction * delta * acceleration
+		velocity -= right_direction * delta * movement_acceleration
 	
 	if Input.is_action_pressed("move_right"):
-		velocity += right_direction * delta * acceleration
-	
-	if Input.is_action_pressed("spacebar"):
-		velocity *= 1.05
+		velocity += right_direction * delta * movement_acceleration
 
 func _input(event):
 	if event is InputEventMouseMotion:
-		if mouse_down:
-			look_velocity.y -= event.relative.x * mouse_sensitivity * acceleration
-			look_velocity.x -= event.relative.y * mouse_sensitivity * acceleration
+		look_velocity.y -= event.relative.x * mouse_sensitivity * look_acceleration
+		look_velocity.x -= event.relative.y * mouse_sensitivity * look_acceleration
+	
+	if event.is_action_pressed("scroll_up"):
+		movement_acceleration = min(2.0, movement_acceleration + 0.05)
 		
+	if event.is_action_pressed("scroll_down"):
+		movement_acceleration = max(0.0, movement_acceleration - 0.05)
