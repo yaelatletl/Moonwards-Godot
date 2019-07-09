c8a45d5bce6d405c3a39cb92594823a37065ad80
Yael Atletl <yaelatletl@gmail.com>
2019-07-09 00:02:55 -0500

    Added Update interface test

---
+++ assets/UI/Menu/Update.gd
@@ -0,0 +1,18 @@
+extends WindowDialog
+var has_update = true
+func _ready():
+	pass # Replace with function body.
+
+func _on_Change_log_pressed():
+	$Change_log.popup_centered()
+
+func _on_Update_check_pressed():
+	$UpdatingUI.popup_centered()
+
+
+func _on_Change_log_confirmed():
+	$Change_log.hide()
+
+
+func _on_Button_pressed():
+	popup_centered()
