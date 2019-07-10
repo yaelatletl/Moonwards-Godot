54a5456b0f7fb0f17cf2da7ad5b4a6ad090143cf
yaelatletl <yael.atletl@gmail.com>
2019-07-10 11:33:54 -0500

    Fixed particles for every camera

--- _tests/Terrain/Particles.gd
+++ _tests/Terrain/Particles.gd
@@ -1,5 +1,6 @@
 extends Particles
 
-#func _process(delta):
-#	if get_tree().get_root().get_camera()!=null:
-#		translation = get_tree().get_root().get_camera().translation
+func _process(delta):
+	var focus = get_tree().get_root().get_camera()
+	if focus !=null:
+		translation = focus.to_global(focus.translation)
