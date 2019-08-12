d7d661a14d0e36350a10465ab134b5f772ec0ca6
Gyrth <gyrthmcmulin@gmail.com>
2019-08-12 21:25:54 +0200

    Removed unused RocketDisplay assets.

--- _tests/RocketDisplay/InfoBox.gd
+++
@@ -1,9 +0,0 @@
-extends Spatial
-
-export (String, MULTILINE)var text
-var text_visible = true
-var active = false
-
-func _ready():
-	$Text/Viewport/InfoBoxContent/Label.text = text
-	get_parent().RegisterInfoBox(self)
--- _tests/RocketDisplay/InfoBoxContent.gd
+++
@@ -1,7 +0,0 @@
-extends VBoxContainer
-
-var info_box_controller
-
-func OnInput(event):
-	if event.is_action_pressed("left_click"):
-		info_box_controller.ToggleVisible()
