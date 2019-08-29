faaa674a7600af9fa635f7fe1b079e621e497ecc
Yael Atletl <yaelatletl@gmail.com>
2019-08-29 13:03:32 -0500

    Camera control typed functions

--- assets/maujoe.camera_control/scripts/camera_control.gd
+++ assets/maujoe.camera_control/scripts/camera_control.gd
@@ -12,7 +12,7 @@ export(int, "Visible", "Hidden", "Caputered, Confined") var mouse_mode : int = 2
 export var mouselook : bool = true
 export (float, 0.0, 1.0) var sensitivity : float = 0.5
 export (float, 0.0, 0.999, 0.001) var smoothness : float = 0.5 setget set_smoothness
-export(NodePath) var privot  setget set_privot
+export(NodePath) var privot setget set_privot
 export var distance = 5.0 setget set_distance
 export var rotate_privot : bool = false
 export var collisions : bool = true setget set_collisions
@@ -38,17 +38,17 @@ export var use_gui : bool = true
 export var gui_action : String = "ui_cancel"
 
 # Intern variables.
-var _mouse_position = Vector2(0.0, 0.0)
-var _yaw = 0.0
-var _pitch = 0.0
-var _total_yaw = 0.0
-var _total_pitch = 0.0
+var _mouse_position : Vector2 = Vector2(0.0, 0.0)
+var _yaw : float = 0.0
+var _pitch : float = 0.0
+var _total_yaw : float = 0.0
+var _total_pitch : float = 0.0
 
-var _direction = Vector3(0.0, 0.0, 0.0)
-var _speed = Vector3(0.0, 0.0, 0.0)
+var _direction : Vector3 = Vector3(0.0, 0.0, 0.0)
+var _speed : Vector3 = Vector3(0.0, 0.0, 0.0)
 var _gui
 
-func _ready():
+func _ready() -> void:
 
 	_check_actions([forward_action, backward_action, left_action, right_action, gui_action, up_action, down_action, ui_mlook])
 
@@ -64,7 +64,7 @@ func _ready():
 		_gui = _gui.new(self, gui_action)
 		add_child(_gui)
 
-func _input(event):
+func _input(event) -> void:
 	if event.is_action_pressed(ui_mlook):
 		mouselook = not mouselook
 		if mouselook:
@@ -112,7 +112,7 @@ func _input(event):
 		elif not Input.is_action_pressed(up_action) and not Input.is_action_pressed(down_action):
 			_direction.y = 0
 
-func _process(delta):
+func _process(delta) -> void:
 	if privot!=null:
 		_update_distance()
 	if mouselook:
@@ -136,7 +136,7 @@ func _process(delta):
 		if not obstacle.empty():
 			set_translation(obstacle.position)
 
-func _update_movement(delta):
+func _update_movement(delta) -> void:
 	var offset = max_speed * acceleration * _direction
 
 	_speed.x = clamp(_speed.x + offset.x, -max_speed.x, max_speed.x)
@@ -156,7 +156,7 @@ func _update_movement(delta):
 	else:
 		global_translate(_speed * delta)
 
-func _update_mouselook():
+func _update_mouselook() -> void:
 	_mouse_position *= sensitivity
 	_yaw = _yaw * smoothness + _mouse_position.x * (1.0 - smoothness)
 	_pitch = _pitch * smoothness + _mouse_position.y * (1.0 - smoothness)
@@ -185,13 +185,13 @@ func _update_mouselook():
 		rotate_y(deg2rad(-_yaw))
 		rotate_object_local(Vector3(1,0,0), deg2rad(-_pitch))
 
-func _update_distance():
+func _update_distance() -> void:
 	if privot!= null:
 		var t = privot.get_translation()
 		t.z -= distance
 		set_translation(t)
 
-func _update_process_func():
+func _update_process_func() -> void:
 	# Use physics process if collision are enabled to ensure it works properly
 	if collisions and privot:
 		set_physics_process(true)
@@ -200,13 +200,13 @@ func _update_process_func():
 		set_physics_process(false)
 		set_process(true)
 
-func _check_actions(actions=[]):
+func _check_actions(actions=[]) -> void:
 	if OS.is_debug_build():
 		for action in actions:
 			if not InputMap.has_action(action):
 				print('WARNING: No action "' + action + '"')
 
-func set_privot(value):
+func set_privot(value) -> void:
 	privot = value
 	# TODO: fix parenting.
 #	if privot:
@@ -215,11 +215,11 @@ func set_privot(value):
 #		privot.add_child(self)
 	_update_process_func()
 
-func set_collisions(value):
+func set_collisions(value) -> void:
 	collisions = value
 	_update_process_func()
 
-func set_enabled(value):
+func set_enabled(value) -> void:
 	enabled = value
 	if enabled:
 		Input.set_mouse_mode(mouse_mode)
@@ -230,8 +230,8 @@ func set_enabled(value):
 		set_process_input(false)
 		set_physics_process(false)
 
-func set_smoothness(value):
+func set_smoothness(value) -> void:
 	smoothness = clamp(value, 0.001, 0.999)
 
-func set_distance(value):
+func set_distance(value) -> void:
 	distance = max(0, value)
