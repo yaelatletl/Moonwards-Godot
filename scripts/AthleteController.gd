92da9562768e4707ede64a070572a4209f80defb
Gyrth <14081613+Gyrth@users.noreply.github.com>
2019-09-20 19:08:17 +0200

    More restyling done on my own scripts. (#75)
    * Restyled rocket display scripts.
    
    * Removed unused rocket display assets.
    
    * Restyled media mode camera script.
    
    * Restyled athlete controller script.
    
    * Restyled avatar preview script in the player settings.
    
    * Script style changes based on TheDuriel's feedback.
    
    * Improved camera control by enabling and disabling the process and input.
    
    * More node caching in the athlete controller script.

--- scripts/AthleteController.gd
+++ scripts/AthleteController.gd
@@ -1,87 +1,98 @@
 extends Spatial
 
-var camera_speed = 5.0
-var movement_speed = 30.0
-var movement_force = Vector3()
-var jump_force = Vector3()
-var look_direction = Vector2()
-var max_speed = 3.0
-var max_up_aim_angle = 55.0
-var max_down_aim_angle = 55.0
-var movement_direction = Vector3(0,0,0)
-var mouse_sensitivity = 0.10
-var turn_speed = 5.0
-var model_quat = Quat()
+const CAMERA_SPEED : float = 5.0
+const MOVEMENT_SPEED : float = 30.0
+const MAX_SPEED : float = 3.0
+const MAX_UP_AIM_ANGLE : float = 55.0
+const MAX_DOWN_AIM_ANGLE : float = 55.0
+const MOUSE_SENSITIVITY : float = 0.10
+const TURN_SPEED : float = 5.0
 
-func _ready():
+onready var camera_position : Node = $KinematicBody/CameraPivot/CameraPosition
+onready var camera : Node = $Camera
+onready var animation_tree : Node = $AnimationTree
+onready var model : Node = $KinematicBody/Scene
+onready var kinematic_body : Node = $KinematicBody
+onready var camera_pivot : Node = $KinematicBody/CameraPivot
+
+var _movement_force : Vector3 = Vector3()
+var _jump_force : Vector3 = Vector3()
+var _look_direction : Vector2 = Vector2()
+var _movement_direction : Vector3 = Vector3(0,0,0)
+var _model_quat : Quat = Quat()
+
+func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
-	movement_direction = global_transform.basis.z
+	_movement_direction = global_transform.basis.z
 
-func _physics_process(delta):
-	var target_transform = $KinematicBody/CameraPivot/CameraPosition.global_transform
-	$Camera.global_transform = $Camera.global_transform.interpolate_with(target_transform, delta * camera_speed)
+func _physics_process(delta : float) -> void:
+	var target_transform = camera_position.global_transform
+	camera.global_transform = camera.global_transform.interpolate_with(target_transform, delta * CAMERA_SPEED)
 	
-	var movement_direction = Vector3()
-	var forward_direction = -$Camera.global_transform.basis.z
+	var _movement_direction = Vector3()
+	var forward_direction = -camera.global_transform.basis.z
 	forward_direction.y = 0.0
-	var right_direction = $Camera.global_transform.basis.x
+	var right_direction = camera.global_transform.basis.x
 	right_direction.y = 0.0
 	
 	if Input.is_action_pressed("move_forwards"):
-		movement_direction += forward_direction
+		_movement_direction += forward_direction
 	
 	if Input.is_action_pressed("move_backwards"):
-		movement_direction -= forward_direction
+		_movement_direction -= forward_direction
 	
 	if Input.is_action_pressed("move_right"):
-		movement_direction += right_direction
+		_movement_direction += right_direction
 	
 	if Input.is_action_pressed("move_left"):
-		movement_direction -= right_direction
+		_movement_direction -= right_direction
 	
-	if (movement_force + movement_direction).length() > max_speed:
-		movement_force = (movement_force + movement_direction).normalized() * max_speed
+	if (_movement_force + _movement_direction).length() > MAX_SPEED:
+		_movement_force = (_movement_force + _movement_direction).normalized() * MAX_SPEED
 	else:
-		movement_force += movement_direction
+		_movement_force += _movement_direction
 	
-	if Input.is_action_just_pressed("jump") and $KinematicBody.is_on_wall():
-		jump_force = Vector3(0.0, 150.0, 0.0)
-		$AnimationTree.set("parameters/Transition/current", "Jump")
+	if Input.is_action_just_pressed("jump") and kinematic_body.is_on_wall():
+		_jump_force = Vector3(0.0, 150.0, 0.0)
+		animation_tree.set("parameters/Transition/current", "Jump")
 	
-	$KinematicBody.move_and_slide((movement_force * movement_speed * delta) + (jump_force * delta) + (Vector3(0.0, -1.62, 0.0)))
-	movement_force *= 0.95
-	jump_force *= 0.95
+	var movement_velocity = _movement_force * MOVEMENT_SPEED * delta
+	var jump_velocity = _jump_force * delta
+	var gravity_velocity = Vector3(0.0, -1.62, 0.0)
+	kinematic_body.move_and_slide(movement_velocity + jump_velocity + gravity_velocity)
 	
-	$KinematicBody/CameraPivot.rotation_degrees = Vector3(look_direction.y, look_direction.x, 0)
+	_movement_force *= 0.95
+	_jump_force *= 0.95
 	
-	var model = $KinematicBody/Scene
-	if movement_force.length() > 0.2:
-		var flat_force = movement_force
+	camera_pivot.rotation_degrees = Vector3(_look_direction.y, _look_direction.x, 0)
+	
+	if _movement_force.length() > 0.2:
+		var flat_force = _movement_force
 		flat_force.y = 0.0
 		flat_force = flat_force.normalized()
 		if flat_force != Vector3():
 			var model_target_transform = model.global_transform.looking_at(model.global_transform.origin - flat_force, Vector3(0, 1, 0))
-			model_quat = model_quat.slerp(Quat(model_target_transform.basis), delta * turn_speed)
-			model.global_transform.basis = Basis(model_quat)
+			_model_quat = _model_quat.slerp(Quat(model_target_transform.basis), delta * TURN_SPEED)
+			model.global_transform.basis = Basis(_model_quat)
 
-func _input(event):
+func _input(event : InputEvent) -> void:
 	if event.is_action_pressed("scroll_up"):
-		$KinematicBody/CameraPivot/CameraPosition.translation.z -= 1.0
-		if $KinematicBody/CameraPivot/CameraPosition.translation.z < 0.0:
-			$KinematicBody/CameraPivot/CameraPosition.translation.z = 0.0
+		camera_position.translation.z -= 1.0
+		if camera_position.translation.z < 0.0:
+			camera_position.translation.z = 0.0
 	
 	if event.is_action_pressed("scroll_down"):
-		$KinematicBody/CameraPivot/CameraPosition.translation.z += 1.0
+		camera_position.translation.z += 1.0
 	
 	if (event is InputEventMouseMotion):
-		look_direction.x -= event.relative.x * mouse_sensitivity
-		look_direction.y -= event.relative.y * mouse_sensitivity
+		_look_direction.x -= event.relative.x * MOUSE_SENSITIVITY
+		_look_direction.y -= event.relative.y * MOUSE_SENSITIVITY
 		
-		if look_direction.x > 360:
-			look_direction.x = 0
-		elif look_direction.x < 0:
-			look_direction.x = 360
-		if look_direction.y > max_up_aim_angle:
-			look_direction.y = max_up_aim_angle
-		elif look_direction.y < -max_down_aim_angle:
-			look_direction.y = -max_down_aim_angle
+		if _look_direction.x > 360:
+			_look_direction.x = 0
+		elif _look_direction.x < 0:
+			_look_direction.x = 360
+		if _look_direction.y > MAX_UP_AIM_ANGLE:
+			_look_direction.y = MAX_UP_AIM_ANGLE
+		elif _look_direction.y < -MAX_DOWN_AIM_ANGLE:
+			_look_direction.y = -MAX_DOWN_AIM_ANGLE
--- scripts/AvatarPreview.gd
+++ scripts/AvatarPreview.gd
@@ -1,53 +1,58 @@
 extends Spatial
 
-export(SpatialMaterial) var base_material
+export(SpatialMaterial) var base_material : SpatialMaterial
 
-var pants_mat
-var shirt_mat
-var skin_mat
-var hair_mat
-var shoes_mat
-var selected 
-var selected_mat = load("res://assets/UI/Options/selected.material")
-var unselected_mat = load("res://assets/UI/Options/unselected.material")
-func _ready():
-	pants_mat = base_material.duplicate()
-	shirt_mat = base_material.duplicate()
-	skin_mat = base_material.duplicate()
-	hair_mat = base_material.duplicate()
-	shoes_mat = base_material.duplicate()
+onready var avatar_female : Node = $AvatarRig/FemaleRig/Skeleton/AvatarFemale
+onready var avatar_skeleton_female : Node = $AvatarRig/FemaleRig/Skeleton
+onready var avatar_male : Node = $AvatarRig/FemaleRig/Skeleton/AvatarMale
+
+var _pants_mat : SpatialMaterial
+var _shirt_mat : SpatialMaterial
+var _skin_mat : SpatialMaterial
+var _hair_mat : SpatialMaterial
+var _shoes_mat : SpatialMaterial
+var _selected_mat : SpatialMaterial = load("res://assets/UI/Options/selected.material")
+var _unselected_mat : SpatialMaterial = load("res://assets/UI/Options/unselected.material")
+
+func _ready() -> void:
+	_pants_mat = base_material.duplicate()
+	_shirt_mat = base_material.duplicate()
+	_skin_mat = base_material.duplicate()
+	_hair_mat = base_material.duplicate()
+	_shoes_mat = base_material.duplicate()
 	
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(1, pants_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(0, shirt_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(2, skin_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(4, hair_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarFemale.set_surface_material(3, shoes_mat)
+	avatar_female.set_surface_material(1, _pants_mat)
+	avatar_female.set_surface_material(0, _shirt_mat)
+	avatar_female.set_surface_material(2, _skin_mat)
+	avatar_female.set_surface_material(4, _hair_mat)
+	avatar_female.set_surface_material(3, _shoes_mat)
 	
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(2, pants_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(3, shirt_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(4, skin_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(1, hair_mat)
-	$AvatarRig/FemaleRig/Skeleton/AvatarMale.set_surface_material(0, shoes_mat)
+	avatar_male.set_surface_material(2, _pants_mat)
+	avatar_male.set_surface_material(3, _shirt_mat)
+	avatar_male.set_surface_material(4, _skin_mat)
+	avatar_male.set_surface_material(1, _hair_mat)
+	avatar_male.set_surface_material(0, _shoes_mat)
 	
-func clean_selected():
-	for genders in $AvatarRig/FemaleRig/Skeleton.get_children():
+func clean_selected() -> void:
+	for genders in avatar_skeleton_female.get_children():
 		for selector in genders.get_children():
-			selector.set_surface_material(0, unselected_mat)
-func set_selected(idx):
-	for genders in $AvatarRig/FemaleRig/Skeleton.get_children():
-		genders.get_child(idx).set_surface_material(0,selected_mat)
+			selector.set_surface_material(0, _unselected_mat)
 
-func SetColors(var pants, var shirt, var skin, var hair, var shoes):
-	pants_mat.albedo_color = pants
-	shirt_mat.albedo_color = shirt
-	skin_mat.albedo_color = skin
-	hair_mat.albedo_color = hair
-	shoes_mat.albedo_color = shoes
+func set_selected(idx : int) -> void:
+	for genders in avatar_skeleton_female.get_children():
+		genders.get_child(idx).set_surface_material(0, _selected_mat)
 
-func SetGender(var gender):
-	if int(gender) == 0:
-		$AvatarRig/FemaleRig/Skeleton/AvatarFemale.show()
-		$AvatarRig/FemaleRig/Skeleton/AvatarMale.hide()
+func set_colors(pants : Color, shirt : Color, skin : Color, hair : Color, shoes : Color) -> void:
+	_pants_mat.albedo_color = pants
+	_shirt_mat.albedo_color = shirt
+	_skin_mat.albedo_color = skin
+	_hair_mat.albedo_color = hair
+	_shoes_mat.albedo_color = shoes
+
+func set_gender(gender : int) -> void:
+	if gender == 0:
+		avatar_female.show()
+		avatar_male.hide()
 	else:
-		$AvatarRig/FemaleRig/Skeleton/AvatarFemale.hide()
-		$AvatarRig/FemaleRig/Skeleton/AvatarMale.show()
+		avatar_female.hide()
+		avatar_male.show()
