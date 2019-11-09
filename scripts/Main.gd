f2afd000cb76928dac1ef8884fd26ae2971d90ac
Siuda <50617077+SiudaPL@users.noreply.github.com>
2019-11-09 15:35:27 +0100

    Issue #108: Headless server launch script (#109)

--- scripts/Main.gd
+++ scripts/Main.gd
@@ -8,6 +8,10 @@ var current_ui = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	if OS.has_feature("Server"):
+		_ready_headless()
+		return
+	
 	_on_size_changed()
 	get_viewport().connect("size_changed",self,"_on_size_changed")
 
--- scripts/MainMenu.gd
+++ scripts/MainMenu.gd
@@ -3,5 +3,19 @@ extends Control
 onready var main_ui : Node = $VBoxContainer
 
 func _ready() -> void:
-	UIManager.register_base_ui(self)
-	UIManager._set_current_ui(main_ui)
+	if OS.has_feature("Server"):
+		_ready_headless()
+	else:
+		UIManager.register_base_ui(self)
+		UIManager._set_current_ui(main_ui)
+
+func _ready_headless() -> void:
+	print("Setup headless mode")
+	var player_data : Dictionary = {
+		name = "Server Bot",
+		options = options.player_opt("server_bot")
+	}
+	gamestate.player_register(player_data, true) #local player
+	gamestate.server_set_mode()
+	var worldscene : String = options.scenes.default_multiplayer_join_server
+	gamestate.change_scene(worldscene)
