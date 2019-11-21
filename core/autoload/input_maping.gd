63768807190a75763e937d4e27cb93bdb8016ea6
yael <yael.atletl@gmail.com>
2019-11-20 21:57:13 -0600

    Fixed input mapping and user settings

--- core/autoload/input_maping.gd
+++ core/autoload/input_maping.gd
@@ -8,23 +8,19 @@ func _ready():
 
 func load_config():
 	for actions in InputMap.get_actions():
-		INPUT_ACTIONS.append(actions)
-	var config = ConfigFile.new()
-	var filecheck = File.new()
-	if not filecheck.file_exists(CONFIG_FILE):
-		config.save(CONFIG_FILE)
-	var err = config.load(CONFIG_FILE)
-	if err: # Assuming that file is missing, generate default config
+		if actions is InputEventKey:
+			INPUT_ACTIONS.append(actions)
+	if options.get('input', 'test') == null:
 		for action_name in INPUT_ACTIONS:
 			var action_list = InputMap.get_action_list(action_name)
 			# There could be multiple actions in the list, but we save the first one by default
 			var scancode = OS.get_scancode_string(action_list[0].scancode)
-			config.set_value("input", action_name, scancode)
-		config.save(CONFIG_FILE)
+			options.set("input", scancode, action_name)
+			options.save()
 	else: # ConfigFile was properly loaded, initialize InputMap
-		for action_name in config.get_section_keys("input"):
+		for action_name in INPUT_ACTIONS:
 			# Get the key scancode corresponding to the saved human-readable string
-			var scancode = config.get_value("input", action_name)
+			var scancode = options.get("input", action_name)
 			# Create a new event object based on the saved scancode
 			var event = InputEventKey.new()
 			event.scancode = scancode
@@ -36,17 +32,8 @@ func load_config():
 
 func save_to_config(section, key, value):
 	"""Helper function to redefine a parameter in the settings file"""
-	
-	var config = ConfigFile.new()
-	
-	var err = config.load(CONFIG_FILE)
-	if err:
-		print("Error code when loading config file: ", err)
-		return -1
-	else:
-		config.set_value(section, key, value)
-		config.save(CONFIG_FILE)
-		return 0
+	options.set(section, value, key)
+	options.save()
 
 func save_all():
 	for action in InputMap.get_actions():
