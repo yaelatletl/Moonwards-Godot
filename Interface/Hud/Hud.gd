85decf1309b26fb402f2c09423fc5b4c96cbda43
TheDuriel <sorker@t-online.de>
2020-01-09 01:21:03 +0100

    Add Chat Hud

--- Interface/Hud/Hud.gd
+++ Interface/Hud/Hud.gd
@@ -3,6 +3,9 @@ extends CanvasLayer
 	Hud Singleton Scene Script
 """
 
+var _active: bool = false
+
+
 func _ready() -> void:
 	hide()
 
@@ -17,3 +20,12 @@ func hide() -> void:
 	for i in get_children():
 		if i is Control:
 			i.visible = false
+
+
+func set_active(state: bool) -> void:
+	_active = state
+	
+	if _active:
+		show()
+	else:
+		hide()
