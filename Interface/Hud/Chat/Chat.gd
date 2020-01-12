696cbf53bca954b9f91520c789f75cd5e0e758ff
Zach777 <MeteorCity3109@protonmail.com>
2020-01-11 21:53:28 -0500

    Chat passes network id to determine username of talker.

--- Interface/Hud/Chat/Chat.gd
+++ Interface/Hud/Chat/Chat.gd
@@ -23,12 +23,7 @@ func _unhandled_input(event: InputEvent) -> void:
 
 func _on_LineEdit_text_entered(new_text: String) -> void:
 	if new_text != "":
-		#TODO: Make below method call pass local player's id instead of
-		#whole username. We do not want a player that hacked there name
-		#to send a rediculously long string and mess up packets for other
-		#players. When GameState gets a get_local_player_id is when this
-		#todo can be finished.
-		rpc("_append_text_to_chat", new_text, Options.username)
+		rpc("_append_text_to_chat", new_text, get_tree().get_network_unique_id() )
 	
 	_le.clear()
 	_le.release_focus()
@@ -37,14 +32,15 @@ func _on_LineEdit_text_entered(new_text: String) -> void:
 	set_deferred("_active", false)
 
 
-remotesync func _append_text_to_chat(new_text: String, talkers_name : String) -> void:
+remotesync func _append_text_to_chat(new_text: String, talker_id : int) -> void:
 	_rtl.newline()
 	# TODO: Add timestap prefix
 	# TODO: Add serverside logging
 	
 	#Show the player's name next to their input.
 	#warning-ignore:return_value_discarded
-	_rtl.append_bbcode( "[color=#DB7900]" + talkers_name +  ":[/color] " )
+	var talker_name : String = GameState.player_get( "username", talker_id )
+	_rtl.append_bbcode( "[color=#DB7900]" + talker_name +  ":[/color] " )
 	
 	#warning-ignore:return_value_discarded
 	_rtl.append_bbcode(new_text)
