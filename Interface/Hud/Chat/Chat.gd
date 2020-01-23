0e74851422bbb6fc57e8258b5fd0c951d2831733
Zach777 <MeteorCity3109@protonmail.com>
2020-01-23 07:39:18 -0500

    Better names for Chat.gd. _rtl->_chat_display_node, _le->_chat_input_node

--- Interface/Hud/Chat/Chat.gd
+++ Interface/Hud/Chat/Chat.gd
@@ -3,8 +3,8 @@ extends PanelContainer
 	Chat Scene Script
 """
 
-onready var _rtl: RichTextLabel = $"V/RichTextLabel"
-onready var _le: LineEdit = $"V/LineEdit"
+onready var _chat_display_node: RichTextLabel = $"V/RichTextLabel"
+onready var _chat_input_node: LineEdit = $"V/LineEdit"
 
 var _active: bool = false
 
@@ -16,8 +16,8 @@ func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey:
 		if event.scancode == KEY_ENTER and event.pressed:
 			if not _active:
-				_le.grab_focus()
-				_le.editable = true
+				_chat_input_node.grab_focus()
+				_chat_input_node.editable = true
 				_active = true
 
 
@@ -25,22 +25,22 @@ func _on_LineEdit_text_entered(new_text: String) -> void:
 	if new_text != "":
 		rpc("_append_text_to_chat", new_text, get_tree().get_network_unique_id() )
 	
-	_le.clear()
-	_le.release_focus()
-	_le.editable = false
+	_chat_input_node.clear()
+	_chat_input_node.release_focus()
+	_chat_input_node.editable = false
 	
 	set_deferred("_active", false)
 
 
 remotesync func _append_text_to_chat(new_text: String, talker_id : int) -> void:
-	_rtl.newline()
+	_chat_display_node.newline()
 	# TODO: Add timestap prefix
 	# TODO: Add serverside logging
 	
 	#Show the player's name next to their input.
 	#warning-ignore:return_value_discarded
 	var talker_name : String = GameState.player_get( "username", talker_id )
-	_rtl.append_bbcode( "[color=#DB7900]" + talker_name +  ":[/color] " )
+	_chat_display_node.append_bbcode( "[color=#DB7900]" + talker_name +  ":[/color] " )
 	
 	#warning-ignore:return_value_discarded
-	_rtl.append_bbcode(new_text)
+	_chat_display_node.append_bbcode(new_text)
